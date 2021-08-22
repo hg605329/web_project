18bb6a91b1aea9f6522f2d9de9ef557494d216be
hg605329 <86401949+hg605329@users.noreply.github.com>
2021-08-22 23:10:20 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,6 +1,7 @@
 $(function(){
 
     $('header').load('index.html header > .area-inner')
-    // $('footer').load('index.html footer > div')
+    $('.area-nav-local').load('company1.html .area-nav-local > .nav-loacl')
+    $('footer').load('company1.html footer > .footer-inner')
 
 });
