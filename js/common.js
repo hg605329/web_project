7713fb95138faca6f2f6ea17afa9b6b5b89b9fdb
hg605329 <86401949+hg605329@users.noreply.github.com>
2021-08-25 13:06:55 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -5,3 +5,22 @@ $(function(){
     $('footer').load('company1.html footer > .footer-inner')
 
 });
+
+
+//헤더 스크롤 이벤트
+$(function(){
+    var lastScrollTop=0,
+    delta= 15;
+    $(window).scroll(function(event){
+        var st= $(this).scrollTop();
+        if(Math.abs(lastScrollTop-st) <= delta)return;
+        if((st> lastScrollTop)&&(lastScrollTop>0)){
+            $(".area-inner").css("top","-112px");
+        }else{
+            $(".area-inner").css("top","0px");
+        }
+        lastScrollTop = st;
+    });
+});
+
+
